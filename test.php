041fabe206ec2d41a0aca47aa1b26a9aef34bdce
Relg99 <relg1999@gmail.com>
2020-08-09 16:30:53 -0500

    test1

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('/var/www/html/prestashop/PrestaShop-webservice-lib-master/PSWebServiceLibrary.php');
+
+try {
+    // creating webservice access
+    $webService = new PrestaShopWebservice('localhost/prestashop', '51LGLPV1LTL934CMJREVA3RZ2ZPQASEG', false);
+ 
+    // call to retrieve all customers
+    $xml = $webService->get(['resource' => 'customers']);
+} catch (PrestaShopWebserviceException $ex) {
+    // Shows a message related to the error
+    echo 'Other error: <br />' . $ex->getMessage();
+}
+
+?>
