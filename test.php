fe6e01fe786a919e6fca4f9e063289c512558b39
Relg99 <relg1999@gmail.com>
2020-08-09 16:34:04 -0500

    test2

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ require_once('/var/www/html/prestashop/PrestaShop-webservice-lib-master/PSWebSer
 
 try {
     // creating webservice access
-    $webService = new PrestaShopWebservice('localhost/prestashop', '51LGLPV1LTL934CMJREVA3RZ2ZPQASEG', false);
+    $webService = new PrestaShopWebservice('34.94.139.183/prestashop', '51LGLPV1LTL934CMJREVA3RZ2ZPQASEG', false);
  
     // call to retrieve all customers
     $xml = $webService->get(['resource' => 'customers']);
